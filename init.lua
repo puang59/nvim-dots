fd85d40badaac270a82c2853c87596c3c5322c0b
puang59 <kk61468@gmail.com>
2025-04-12 16:45:04 +0530

    feat: nvim tree

--- init.lua
+++ init.lua
@@ -12,7 +12,8 @@ vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
 vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
 vim.keymap.set('n', 'grr', vim.lsp.buf.references)
 
-vim.keymap.set('n', '<space>on', '<cmd>Oil<CR>')
+vim.keymap.set('n', '<space>e', '<cmd>Oil<CR>')
 vim.keymap.set('n', '<leader>ot', '<cmd>Floaterminal<cr>')
 
 vim.keymap.set("n", "<C-x>3", "<cmd>vsplit<CR>", { desc = "Vertical split" })
+vim.keymap.set("n", "<leader>on", "<cmd>NvimTreeToggle<CR>")
